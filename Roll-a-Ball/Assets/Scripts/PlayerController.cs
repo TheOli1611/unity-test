e192eedad31a23e449ca44f9bb91e019d087b6f6
TheOli1611 <theoli1611@gmail.com>
2025-04-09 13:55:26 -0400

    starting ai section

--- Roll-a-Ball/Assets/Scripts/PlayerController.cs
+++ Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -2,18 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb; 
+    private int count;
     private float movementX;
     private float movementY;
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent <Rigidbody>(); 
+        count = 0; 
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -35,6 +42,17 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp")) 
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText() 
+    {
+        countText.text =  "Count: " + count.ToString();
+        if (count >= 4)
+        {
+            winTextObject.SetActive(true);
         }
     }
 }
