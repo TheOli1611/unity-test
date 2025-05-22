8f070c9809f4977b100cab901b2fad67dbe51166
TheOli1611 <theoli1611@gmail.com>
2025-05-22 13:54:37 -0400

    IT WORKS

---
+++ clicker game/Assets/Main.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Main : MonoBehaviour
+{
+    public Double CDCount;
+    public Double requiredAmount = 20;
+    public int timesUpgraded = 1;
+    public Button upgradeAddButton;
+    public GameObject upgradeShopUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CDCount = 0;
+        upgradeAddButton.interactable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        requiredAmount = Math.Round(20 * Math.Pow(2.5, timesUpgraded));
+        if (CDCount >= requiredAmount)
+        {
+            upgradeAddButton.interactable = true;
+        }
+        else
+        {
+            upgradeAddButton.interactable = false;
+        }
+        
+    }
+
+    public void AddCD()
+    {
+        CDCount = CDCount + 1 + timesUpgraded;
+        Debug.Log(CDCount);
+    }
+
+    public void UpgradeShopOpen()
+    {
+        upgradeShopUI.SetActive(true);
+    }
+
+    public void Upgrade1()
+    {
+        if (CDCount >= requiredAmount)
+        {
+            CDCount = CDCount - requiredAmount;
+            Debug.Log("New Total" + CDCount);
+            timesUpgraded += 1;
+        }
+    }
+}
